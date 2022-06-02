d456a3cc00287678197f13342e51ad9279fbba03
kevin <kj.fortuin@gmail.com>
2022-06-02 15:30:20 +0200

    6-size.c
    Signed-off-by: kevin <kj.fortuin@gmail.com>

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,10 +15,10 @@ int main(void)
     float Thefloat;
 
     // sizeof evaluates the size of a variable
-    printf("Size of char: %zu bytes\n", sizeof(TheCharacter));
-    printf("Size of int: %zu bytes\n", sizeof(TheInteger));
-    printf("Size of long: %zu bytes\n", sizeof(TheLong));
-    printf("Size of long long: %zu byte\n", sizeof(TheLongLong));
+    printf("Size of char: %zu bytes(s)\n", sizeof(TheCharacter));
+    printf("Size of int: %zu bytes(s)\n", sizeof(TheInteger));
+    printf("Size of long: %zu bytes(s)\n", sizeof(TheLong));
+    printf("Size of long long: %zu byte(s)\n", sizeof(TheLongLong));
     
     return 0;
 }
