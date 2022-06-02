c9a7a7376a8b64f009e907056cc9d2da80ee6846
kevin <kj.fortuin@gmail.com>
2022-06-02 15:36:04 +0200

    6-size.c
    Signed-off-by: kevin <kj.fortuin@gmail.com>

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,17 +8,17 @@
 
 int main(void)
 {
-	char acharacter;
-	int ainteger;
-	long along;
-	long long alonglong;
-	float afloat;
+	char Thecharacter;
+	int Theinteger;
+	long Thelong;
+	long long Thelonglong;
+	float Thefloat;
 
-	printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
-	printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(along));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
-	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
+	printf("Size of a char: %lu byte(s)\n", sizeof(Tcharacter));
+	printf("Size of an int: %lu byte(s)\n", sizeof(Tinteger));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(Tlong));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(Tlonglong));
+	printf("Size of a float: %lu byte(s)\n", sizeof(Tfloat));
 
 	return (0);
 }
