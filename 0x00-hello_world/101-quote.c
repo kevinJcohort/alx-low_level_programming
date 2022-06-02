be2448f9592693492353b55e0d063d7bfce49d6e
kevin <kj.fortuin@gmail.com>
2022-06-02 22:37:33 +0200

    100-intel
    Signed-off-by: kevin <kj.fortuin@gmail.com>

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -8,7 +9,7 @@
 
 int main(void)
 {
-    write ("and that piece of art is useful" - Dora Korpar, 2015-10-19"
+    write (1, "and that piece of art is useful" - Dora Korpar, 2015-10-19"
     );
-return (0);
+return (1);
 }
