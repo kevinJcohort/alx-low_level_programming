48d4681823d2a28a437b1512ec4d55e9d35898ae
kevin <kj.fortuin@gmail.com>
2022-06-03 20:55:04 +0200

    9-print_comb.c
    Signed-off-by: kevin <kj.fortuin@gmail.com>

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * prints all possible combinations of single-digit no.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar((n % 10) + '0');
+		if (n == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+	return (0);
+}
