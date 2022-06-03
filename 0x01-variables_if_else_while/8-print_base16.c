2780bbec3e487b81352d2dbcd4dda7899a2c8255
kevin <kj.fortuin@gmail.com>
2022-06-03 20:51:59 +0200

    8-print_base16.c
    Signed-off-by: kevin <kj.fortuin@gmail.com>

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * prints numbers of base16 in lowercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int dig, letterhex, n;
+
+	for (dig = 48, n = 0; n < 10; dig++, n++)
+	{
+		putchar(dig);
+	}
+	for (letterhex = 97, n = 1; n < 7; letterhex++, n++)
+	{
+		putchar(letterhex);
+	}
+	putchar(10);
+	return (0);
+}
