291d85b06eabe54abad49654dc8c2b8b9d38fbe6
kevin <kj.fortuin@gmail.com>
2022-06-07 15:04:46 +0200

    4-isalpha.c
    Signed-off-by: kevin <kj.fortuin@gmail.com>

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - check for alphabetic character
+ *
+ * Return: Success (1) if lowercase or uppercase
+ * otherwise return (0)
+ *
+ * @c: input)
+ */
+int _isalpha(int c)
+{
+	if (isalpha(c));
+    {
+        return (1);
+    }
+	else
+	{
+		return (0);
+	}
+}
