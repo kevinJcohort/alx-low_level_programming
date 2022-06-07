e6cdb42983b32fbe5d7cf6966533abf0cbf4673e
kevin <kj.fortuin@gmail.com>
2022-06-07 23:39:48 +0200

    4-isalpha.c
    Signed-off-by: kevin <kj.fortuin@gmail.com>

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
-#include <ctype.h>
+#include "main.h"
+
 /**
  * _isalpha - check for alphabetic character
  *
@@ -10,12 +10,5 @@
  */
 int _isalpha(int c)
 {
-    if (isalpha(c))
-    {
-    return (1);
-    }
-    else
-    {
-    return (0);
-    }
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
