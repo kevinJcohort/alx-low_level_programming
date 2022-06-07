328051b5ffb38ff4cc9b16ad57e0bc160a35f887
kevin <kj.fortuin@gmail.com>
2022-06-07 11:22:46 +0200

    0-putchar.c
    Signed-off-by: kevin <kj.fortuin@gmail.com>

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/*
+* Main-This prints the word _putchar using putchar
+*
+*
+*/
+
+int main()
+{  
+    // Write the Character to stdout
+    putchar('_');
+    putchar('P');
+    putchar('u');
+    putchar('t');
+    putchar('c');
+    putchar('h');
+    putchar('a');
+    putchar('r');
+  
+    return (0);
+}
