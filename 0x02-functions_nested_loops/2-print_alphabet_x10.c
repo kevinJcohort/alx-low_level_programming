164254e78d3cde12bbb8a4242fef388e8e6e38e7
kevin <kj.fortuin@gmail.com>
2022-06-07 12:52:47 +0200

    2-print_alphabet_x10.c
    Signed-off-by: kevin <kj.fortuin@gmail.com>

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,13 +12,13 @@ void print_alphabet_x10(void)
     {
         {char c = 'a'
     
-        while(c<= 'z')
-        {
+            while(c <= 'z')
+            {
             _putchar(c);
             c++;
+            }
+            _putchar('\n');
+            i++;
         }
-        _putchar('\n');
-        i++;
-        }
-    
+    }
 }
