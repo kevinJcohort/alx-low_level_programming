cf187b695067f29dc0ff33640c1308c98bb62cbb
kevin <kj.fortuin@gmail.com>
2022-06-07 12:58:10 +0200

    2-print_alphabet_x10.c
    Signed-off-by: kevin <kj.fortuin@gmail.com>

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,24 +1,25 @@
 #include "main.h"
-/**
- * main- prints the alphabet 10x 
- * 
- * return: 0
- */
 
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ *
+ * Return 0
+ */
 void print_alphabet_x10(void)
 {
-    int i =0;
-    while  (i = 0; i < 10; i++)
-    {
-        {char c = 'a'
-    
-            while( c <= 'z')
-            {
-            _putchar(c);
-            c++;
-            }
-            _putchar('\n');
-            i++;
-        }
-    }
+	int i = 0;
+
+	while (i < 10)
+	{
+		char c = 'a';
+
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+
+		_putchar('\n');
+		i++;
+	}
 }
