04f12e4e26d357526a133e1fafcfeac90f384293
kevin <kj.fortuin@gmail.com>
2022-06-07 16:24:14 +0200

    6-abs.c
    Signed-off-by: kevin <kj.fortuin@gmail.com>

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- *_abs - computes absolute valve of an integer
+ * _abs - computes absolute value of an integer
+ * @x: integer
  *Return: 0
- *
- *@x: integer
  */
 int _abs(int x)
 {
-if (x < 0)
-{
-    return (x* -1);
-}
-return (x);
+
+	if (x < 0)
+	{
+		return (x * -1);
+	}
+
+	return (x);
 }
