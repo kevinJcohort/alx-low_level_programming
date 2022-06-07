749ad05d89f3542595f79423ea63475da6bf127a
kevin <kj.fortuin@gmail.com>
2022-06-07 12:26:15 +0200

    1-alphabet.c
    Signed-off-by: kevin <kj.fortuin@gmail.com>

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,19 @@
 #include "main.h"
 
 /**
- * Main-prints the alphabet in lowercase
- * 
- * return 0
+ * print_alphabet - Entry point
+ *
+ * Return: 0
  */
-
-void print_alphabet(void);
+void print_alphabet(void)
 {
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
 
 }
