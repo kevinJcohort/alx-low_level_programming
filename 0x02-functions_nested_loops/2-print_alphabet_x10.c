cdca88d606cac745185c9635e8634d4c21d35187
kevin <kj.fortuin@gmail.com>
2022-06-07 12:42:58 +0200

    2-print_alphabet_x10.c
    Signed-off-by: kevin <kj.fortuin@gmail.com>

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main- prints the alphabet 10x 
+ * 
+ * return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+    int i
+    while(i = 0; i < 10; i++);
+    
+    {
+        char c = 'a';
+        while(c<='z')
+        {
+            _putchar(c);
+            c++;
+        }
+        _putchar('\n');
+    }
+}
