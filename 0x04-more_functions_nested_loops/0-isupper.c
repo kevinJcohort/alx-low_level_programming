24cf5c0c4733b14038f6bad8057652f6ba85cc85
kevin <kj.fortuin@gmail.com>
2022-06-09 13:46:23 +0200

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,20 +2,21 @@
 #include <stdio.h>
 
 /**
- * main - check for upper case characters and return 1 if true and 0 if false 
- * return: int
+ *main - check for upper case characters and return 1 if true and 0 if false
+ *return: int
  *
  *@c: input
  */
 
 int _isupper(char c)
-
-if (insupper(c))
 {
-return: 1;
-}
-else 
-{
-return: 0;
+	if (insupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
 
