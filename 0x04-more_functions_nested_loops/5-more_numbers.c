ff9c8aa735c871cd1a9842ed9bf2fce32481219e
kevin <kj.fortuin@gmail.com>
2022-06-09 16:03:11 +0200

    5-more_numbers.c
    Signed-off-by: kevin <kj.fortuin@gmail.com>

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+#include "main.h"
+/**
+ * more_numbers - prints 10 times the numbers 0 to 14
+ *
+ * Return: 0
+ */
+int i, x
+
+void more_numbers(void)
+{
+	if (i = 0; i < 10; i++)
+	{
+		for(x = 0;x < 15; x++)
+		{
+			_putchar('0', x++);
+		}
+		_putchar('0', i++);
+	}
+
+	_putchar('\n');
+}
