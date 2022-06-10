f4eea59e0c10497c5c75108c3181f4648b4b43c4
kevin <kj.fortuin@gmail.com>
2022-06-10 13:59:01 +0200

    100-prime_factor.c
    Signed-off-by: kevin <kj.fortuin@gmail.com>

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+
+int main()
+{
+	int input = 612852475143;
+	int isPrime = true
+
+	for (int i = sqrt(input); i > 1; i--)
+	{
+		printf("%d", i);
+		if (input % i == 0)
+		{
+			isPrime = false
+		}
+	}
+	if (isPrime)
+	{
+		printf("%d\n")
+	}
+}
