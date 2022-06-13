c784bbcf1e3293058f678fea836fe95035239692
kevin <kj.fortuin@gmail.com>
2022-06-13 14:18:40 +0200

    4-print_rev.c
    Signed-off-by: kevin <kj.fortuin@gmail.com>

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,8 +13,9 @@ void print_rev(char *s)
 	int length = strlen(s);
 	int middle = length / 2;
 	char temp;
+	int i;
 
-	for (int i = 0; i < middle; i++)
+	for (i = 0; i < middle; i++)
 	{
 		temp - s[i];
 		s[i] = s[length - i - 1];
