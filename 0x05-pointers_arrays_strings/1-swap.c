b5f94de2d2ac15a6b3d9e15b00290fe8e16333e4
kevin <kj.fortuin@gmail.com>
2022-06-13 11:56:49 +0200

    1-swap.c
    Signed-off-by: kevin <kj.fortuin@gmail.com>

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: pointer a
+ * @b: pointer b
+ *
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = *a - *b;
+	*b = *a + *b;
+	*a = *b - *a;
+}
