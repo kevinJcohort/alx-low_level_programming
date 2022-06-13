56f36903ef0b9cea1fd2e6644a4515e0d4303847
kevin <kj.fortuin@gmail.com>
2022-06-13 14:10:10 +0200

    4-print_rev.c
    Signed-off-by: kevin <kj.fortuin@gmail.com>

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - prints string in reverse
+ * @s: string to be printed
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int length = strlen(s);
+	int middle = length / 2;
+	char temp;
+
+	for (int i = 0; i < middle; i++)
+	{
+		temp - s[i];
+		s[i] = s[length - i - 1];
+		s[length - i - 1] = temp;
+	}
+}
