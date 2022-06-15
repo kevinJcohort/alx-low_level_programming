5787098aef90ecc83d140b05be5f619f3c7fe151
kevin <kj.fortuin@gmail.com>
2022-06-15 09:29:51 +0200

    0-strcat.c
    Signed-off-by: kevin <kj.fortuin@gmail.com>

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * char *_strcat - appends the src string to the dest
+ * 
+ * returns: 0
+ * 
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+	for (i = 0;*dest != '\0'; i++);
+	for (j=0; *src != '\0'; j++)
+		dest[i+j] = src[j];
+		dest[i=j] = '\0';
+		
+		return dest;
+}
